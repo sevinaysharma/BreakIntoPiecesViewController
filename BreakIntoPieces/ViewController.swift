9b1982ab09b9f7d7962bc0b7893209082d274cdd
Vinay Sharma <sevinaysharma>
2022-05-17 23:33:17 +0530

    Initial Commit

---
+++ BreakIntoPieces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BreakIntoPieces
+//
+//  Created by Vinay Sharma on 17/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BreakIntoPiecesUITests/BreakIntoPiecesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BreakIntoPiecesUITestsLaunchTests.swift
+//  BreakIntoPiecesUITests
+//
+//  Created by Vinay Sharma on 17/05/22.
+//
+
+import XCTest
+
+class BreakIntoPiecesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
